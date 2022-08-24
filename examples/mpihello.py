f2cb5bb7b176589e576022f3cecf2ed78fc30e4c
David Henty <d.henty@epcc.ed.ac.uk>
2022-08-24 15:51:56 +0100

    Updated .

---
+++ examples/mpihello.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import numpy as np
+from mpi4py import MPI
+
+comm = MPI.COMM_WORLD
+
+size = comm.Get_size()
+rank = comm.Get_rank()
+
+print(f"Hello from rank {rank} out of {size}")
